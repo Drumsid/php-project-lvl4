65f2e45d95742599165840aa0ae2da51f3600044
densidnay@gmail.com <Jesuschrist05882>
2021-03-05 12:15:40 +0300

    test auto deploy

--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -15,7 +15,6 @@ class TaskStatus extends Model
 
     public function task()
     {
-        // return $this->hasMany(Task::class, 'status_id');
         return $this->hasMany(Task::class);
     }
 }
