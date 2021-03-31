93c88d2f4fa9d3c59115a634746736c4b06f5f2e
densidnay@gmail.com <Jesuschrist05882>
2021-03-31 11:13:01 +0300

    fix traits in auth modul

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
+    use RegistersUsers;
     /*
     |--------------------------------------------------------------------------
     | Register Controller
@@ -21,8 +22,6 @@ class RegisterController extends Controller
     | provide this functionality without requiring any additional code.
     |
     */
-
-    use RegistersUsers;
 
     /**
      * Where to redirect users after registration.
